563e7721d7f6b2e2637f1497327187fc8620e581
shivamalgoworks <93613470+shivamalgoworks@users.noreply.github.com>
2022-08-07 13:04:41 +0530

    added search functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
-import type { NextPage } from "next";
+import React from "react";
 import axios from "axios";
+
 import VideoCard from "../components/VideoCard";
+import { BASE_URL } from "../utils";
 import { Video } from "../types";
 import NoResults from "../components/NoResults";
-import { BASE_URL } from "../utils";
 
 interface IProps {
   videos: Video[];
 }
+
 const Home = ({ videos }: IProps) => {
   return (
     <div className="flex flex-col gap-10 videos h-full">
@@ -24,10 +26,18 @@ const Home = ({ videos }: IProps) => {
 
 export default Home;
 
-export const getServerSideProps = async () => {
-  const { data } = await axios.get(`${BASE_URL}/api/post`);
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response = await axios.get(`${BASE_URL}/api/post`);
+
+  if (topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  }
 
   return {
-    props: { videos: data },
+    props: { videos: response.data },
   };
 };
